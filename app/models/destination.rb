0418895cd6814ad5824b66aff2def70f31f4e145
Milan <milanm1120@gmail.com>
2021-11-23 13:36:40 -0500

    set up relationships in User/Pin/Destination models

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,4 @@
 class Destination < ApplicationRecord
+    has_many :pins
+    has_many :users, through: :pins
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :pins
+  has_many :destinations, through: :pins
   has_secure_password
 end
