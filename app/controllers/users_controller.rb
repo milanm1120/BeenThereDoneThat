2b35a813fdc104b4b5e6355e6541101486341ff3
Milan <milanm1120@gmail.com>
2021-11-23 19:15:17 -0500

    create views/users/new.html.erb page and generic login, users_controller method create and strong params set, working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,17 @@ class UsersController < ApplicationController
     end
 
     def show
-
+        
     end
 
     def create
-
+        @user = User.new(user_params)
+        # if @user.save
+        #     session[:user_id] = @user.id            #sessions[:user_id] is used to login
+        #     redirect_to user_path(@user)
+        # else
+        #     render :new
+        # end
     end
 
     def edit
@@ -28,5 +34,9 @@ class UsersController < ApplicationController
 
     end
 
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
 end
- >>
