0c7631040963ad4b067299ba20d5930ba7afb2e3
Milan <milanm1120@gmail.com>
2021-11-23 21:22:45 -0500

    build out destinations_controller and add views/destinations/index.html.erb

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,44 @@
 class DestinationsController < ApplicationController
+
+    def index
+        @destinations = Destination.all
+    end
+
+    def new     #render a new form
+        @destination = Destination.new
+    end
+
+    def show
+        @destination = Destination.find_by_id(params[:id])
+    end
+
+    def create
+        @destination = Destination.new(destination_params)
+        if @pin.save
+            redirect_to destination_path(@destination)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @destination = Destination.find_by_id(params[:id])
+    end
+
+    def update
+        @destination = Destination.find_by_id(params[:id])
+        @destination.update(params[:destination])
+    end
+
+    def destroy
+        @destination = Destination.find_by_id(params[:id])
+        @destination.destroy
+            redirect_to destinations_path
+    end
+
+    private
+    def destination_params      #strong params which permits fields being created
+        params.require(:destination).permit(:city, :country)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
+  validate :valid_email?
+end
+
+#custom methods
+
+def valid_email?
+  if !email.match(/\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+    errors.add(:email, "Invalid, Please Try Again")
+  end
 end
