7d06424d6b4a6dd8c8531aae45d2a25aeb736d60
Milan <milanm1120@gmail.com>
2021-11-23 19:53:56 -0500

    add 'display validation errors in view' code to view/layouts/_error.html.erb, add .field_with_errors code to stylesheets/application.css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,11 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        byebug
-        # if @user.save
-        #     session[:user_id] = @user.id            #sessions[:user_id] is used to login
-        #     redirect_to user_path(@user)
-        # else
-        #     render :new
-        # end
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def edit
